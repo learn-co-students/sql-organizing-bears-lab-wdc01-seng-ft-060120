3a0120057a66cc3f8d7e87cd3acc0d83c9fb0022
steve <stevepesce879@gmail.com>
2020-06-11 19:52:22 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        TEXT,
+    age         INT,
+    gender      TEXT,
+    color       TEXT,
+    temperament TEXT,
+    alive       INT
+);
